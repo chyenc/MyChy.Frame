7acebf22c7a1da842555d487605c37b23b80fc39
陈岩 <chyenx@gmail.com>
2015-11-27 13:40:14 +0800

    拆分MyChy.Frame.Common.Data 企业库操作单独引用

---
+++ MyChy.Frame.Common.Data/DataHelperBase.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+namespace MyChy.Frame.Common.Data
+{
+    /// <summary>
+    /// OracleDataBase 的摘要说明
+    /// </summary>
+    public class DataHelperBase
+    {
+        /// <summary>
+        /// 执行DbCommand 返回list类
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <param name="cmd"></param>
+        /// <returns></returns>
+        public static IList<T> GetListEntity<T>(Database db, DbCommand cmd)
+        {
+            using (var ds = db.ExecuteDataSet(cmd))
+            {
+                return ModelHelper.GetListModelByTable<T>(ds.Tables[0]);
+            }
+        }
+
+        /// <summary>
+        /// 执行DbCommand 返回类
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <param name="cmd"></param>
+        /// <returns></returns>
+        public static T GetEntity<T>(Database db, DbCommand cmd)
+        {
+            using (var ds = db.ExecuteDataSet(cmd))
+            {
+                return ModelHelper.GetModelByTable<T>(ds.Tables[0]);
+            }
+        }
+
+        /// <summary>
+        /// 执行sqltxt 返回list类
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <param name="sqltxt"></param>
+        /// <returns></returns>
+        public static IList<T> GetListEntity<T>(Database db, string sqltxt)
+        {
+            return GetListEntity<T>(db, sqltxt, null);
+        }
+
+        /// <summary>
+        /// 执行sqltxt 返回类
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <param name="sqltxt"></param>
+        /// <returns></returns>
+        public static T GetEntity<T>(Database db, string sqltxt)
+        {
+            return GetEntity<T>(db, sqltxt, null);
+        }
+
+        /// <summary>
+        /// 执行sqltxt 返回list类 带参数
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <param name="sqltxt"></param>
+        /// <param name="parms"></param>
+        /// <returns></returns>
+        public static IList<T> GetListEntity<T>(Database db, string sqltxt, SqlParameter[] parms)
+        {
+            var da = GetDataTable(db, sqltxt, parms);
+            return ModelHelper.GetListModelByTable<T>(da);
+        }
+
+        /// <summary>
+        /// 执行sqltxt 返回类 带参数
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <param name="sqltxt"></param>
+        /// <param name="parms"></param>
+        /// <returns></returns>
+        public static T GetEntity<T>(Database db, string sqltxt, SqlParameter[] parms)
+        {
+            var da = GetDataTable(db, sqltxt, parms);
+            return ModelHelper.GetModelByTable<T>(da);
+        }
+
+        /// <summary>
+        /// 执行sqltxt 返回Table
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="sqltxt"></param>
+        /// <param name="parms"></param>
+        /// <returns></returns>
+        public static DataTable GetDataTable(Database db, string sqltxt, SqlParameter[] parms)
+        {
+            DataTable data = null;
+            var set = GetDataSet(db, sqltxt, parms);
+            if (set != null && set.Tables.Count > 0)
+            {
+                data = set.Tables[0];
+            }
+            return data;
+        }
+
+        /// <summary>
+        /// 执行sqltxt 返回DataSet
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="sqltxt"></param>
+        /// <param name="parms"></param>
+        /// <returns></returns>
+        public static DataSet GetDataSet(Database db, string sqltxt, SqlParameter[] parms)
+        {
+            DataSet ds;
+            using (var cmd = db.GetSqlStringCommand(sqltxt))
+            {
+                if (parms != null)
+                {
+                    foreach (var i in parms)
+                    {
+                        db.AddInParameter(cmd, i.ParameterName, i.DbType, i.Value);
+                    }
+                }
+                ds = db.ExecuteDataSet(cmd);
+            }
+            return ds;
+        }
+
+        /// <summary>
+        ///  执行sqltxt 返回Scalar
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <param name="sqltxt"></param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public static T ExecuteScalar<T>(Database db, string sqltxt, T def)
+        {
+            return ExecuteScalar<T>(db, sqltxt, null, def);
+        }
+
+        /// <summary>
+        /// 执行sqltxt 返回Scalar 返回默认值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="db"></param>
+        /// <param name="sqltxt"></param>
+        /// <param name="parms"></param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public static T ExecuteScalar<T>(Database db, string sqltxt, SqlParameter[] parms, T def)
+        {
+
+            object obj = null;
+            using (var cmd = db.GetSqlStringCommand(sqltxt))
+            {
+                if (parms != null)
+                {
+                    foreach (var i in parms)
+                    {
+                        if (i.Value != null)
+                        {
+                            db.AddInParameter(cmd, i.ParameterName, i.DbType, i.Value);
+                        }
+                    }
+                }
+                obj = db.ExecuteScalar(cmd);
+            }
+            return obj.To<T>(def);
+        }
+
+        /// <summary>
+        /// 执行sqltxt 返回NonQuery
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="sqltxt"></param>
+        /// <returns></returns>
+        public static int ExecuteNonQuery(Database db, string sqltxt)
+        {
+            return ExecuteNonQuery(db, sqltxt, null);
+        }
+
+        /// <summary>
+        /// 执行sqltxt 返回NonQuery  带参数
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="sqltxt"></param>
+        /// <param name="parms"></param>
+        /// <returns></returns>
+        public static int ExecuteNonQuery(Database db, string sqltxt, SqlParameter[] parms)
+        {
+            using (var cmd = db.GetSqlStringCommand(sqltxt))
+            {
+                if (parms == null) return db.ExecuteNonQuery(cmd);
+                foreach (var i in parms)
+                {
+                    if (i.Value != null)
+                    {
+                        db.AddInParameter(cmd, i.ParameterName, i.DbType, i.Value);
+                    }
+                }
+                return db.ExecuteNonQuery(cmd);
+            }
+        }
+    }
+}
--- MyChy.Frame.Common.Data/Properties/AssemblyInfo.cs
+++ MyChy.Frame.Common.Data/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("MyChy.Frame.Common.Redis")]
-[assembly: AssemblyDescription("去除没有必要引用")]
+[assembly: AssemblyTitle("MyChy.Frame.Common.Data")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MyChy")]
-[assembly: AssemblyProduct("MyChy.Frame.Common.Redis")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyChy.Frame.Common.Data")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("997f9127-4fab-43cf-aa63-9c8ec1e6f929")]
+[assembly: Guid("d062a5bd-1a21-4444-8028-436588252d78")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
