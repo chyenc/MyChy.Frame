f2d2a762637af6f6df888fc031290b7f6493b374
陈岩 <chyenx@gmail.com>
2016-08-23 10:10:54 +0800

    增加MyChy.Frame.Common.EfData

--- nugut.bat
+++ nugut.bat
@@ -2,4 +2,6 @@ del *.nupkg
 nuget pack MyChy.Frame.Common\MyChy.Frame.Common.csproj  -Prop Configuration=Release
 nuget pack MyChy.Frame.Common.Redis\MyChy.Frame.Common.Redis.csproj  -Prop Configuration=Release
 nuget pack MyChy.Frame.Common.Data\MyChy.Frame.Common.Data.csproj  -Prop Configuration=Release
+nuget pack MyChy.Frame.Common.EfData\MyChy.Frame.Common.EfData.csproj  -Prop Configuration=Release
+
 nuget push *.nupkg -s http://nuget.chyenc.com 8bfc20c1-83a6-4d0f-a48e-209a8dda7ad2
